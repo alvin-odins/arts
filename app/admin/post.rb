2c6648a64594ff0ce2b817ba2c3eb478bb4393b0
alvin <alvin.odins@gmail.com>
2017-08-14 02:18:15 -0700

    comment out add/admin/post.rb

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Post do
+# ActiveAdmin.register Post do
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,22 +14,22 @@ ActiveAdmin.register Post do
 # end
 
 
-  index do
+  # index do
     # column :category
-    column :title
-    column :description
-    column :measurement
-    column :artist
-    column :category
-    column "Posted", :created_at
-    column "Modified", :updated_at
+    # column :title
+    # column :description
+    # column :measurement
+    # column :artist
+    # column :category
+    # column "Posted", :created_at
+    # column "Modified", :updated_at
     # column :image_file_name
     # column :image_content_type
     # column :image_file_size
     # column :image_pdated_at
-    column :image
-    actions
-  end
+  #   column :image
+  #   actions
+  # end
 
   # index :as => :grid do |post|
   #   div do
@@ -40,37 +40,37 @@ ActiveAdmin.register Post do
   #   a truncate(post.title), :href => admin_post_path(post)
   # end
 
-  form html: { enctype: "multipart/form-data" } do |f|
-    f.inputs "New Post", multipart: true do
-      f.input :title, required: true
-      f.input :description, required: true
-      f.input :measurement, required: true
-      f.input :artist, required: true
-      f.input :category, required: true
-      f.input :image, required: true, as: :file
-    end
-    f.actions
-  end
-
-  show do
-    attributes_table do
-      row :title
-      row :description
-      row :measurement
-      row :artist
-      row :category
-      row :image do
-        image_tag post.image.url
-      end
-    end
-    active_admin_comments
-  end
-
-  controller do
-    def edit
-      @post = Post.find(params[:id])
-      render layout: false
-    end
+  # form html: { enctype: "multipart/form-data" } do |f|
+  #   f.inputs "New Post", multipart: true do
+  #     f.input :title, required: true
+  #     f.input :description, required: true
+  #     f.input :measurement, required: true
+  #     f.input :artist, required: true
+  #     f.input :category, required: true
+  #     f.input :image, required: true, as: :file
+  #   end
+  #   f.actions
+  # end
+  #
+  # show do
+  #   attributes_table do
+  #     row :title
+  #     row :description
+  #     row :measurement
+  #     row :artist
+  #     row :category
+  #     row :image do
+  #       image_tag post.image.url
+  #     end
+  #   end
+  #   active_admin_comments
+  # end
+  #
+  # controller do
+  #   def edit
+  #     @post = Post.find(params[:id])
+  #     render layout: false
+  #   end
 
     # def update
     #   @post = Post.find(params[:id])
@@ -78,8 +78,8 @@ ActiveAdmin.register Post do
     #   render layout: false
     # end
 
-  end
-
-  permit_params :title, :description, :measurement, :artist, :category, :image
-
-end
+#   end
+#
+#   permit_params :title, :description, :measurement, :artist, :category, :image
+#
+# end
